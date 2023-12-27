0445c1ca1a8564d0991cf42cfdc169aa7359bce5
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-12-27 22:49:30 +0800

    feat: database connection to mysql

---
+++ database/index.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+// create the pool
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME
+})
+
+module.exports = pool.promise();
