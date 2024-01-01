9124df3ff216ce2988b4d1dad7225caac63f8c6e
Jeremy Opriasa <opriasajbusiness@gmail.com>
2024-01-01 21:52:05 +0800

    delete: these files has been deleted due to renaming the files for cleanliness of the code

--- routes/login.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const loginController = require("../controller/login.controller")
-
-router.get("/", loginController.get)
-
-module.exports = router;
