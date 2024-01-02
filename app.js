855b79cb88d176a3af37d12b2f9fa3575ea7b0c8
Jeremy Opriasa <opriasajbusiness@gmail.com>
2024-01-02 22:58:16 +0800

    fix: add the employee router endpoint

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const app = express()
 require("dotenv").config()
 
 const menuRouter = require("./routes/menu.routes")
+const employeeRouter = require("./routes/employee.routes")
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 app.use("/restapi/restaurant/menu", menuRouter)
+app.use("/restapi/restaurant/employee", employeeRouter)
 
 const PORT = process.env.PORT || 3000
 
