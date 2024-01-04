a926f65d3c99ad1572d95bbe217d8edaadbf5411
Jeremy Opriasa <opriasajbusiness@gmail.com>
2024-01-04 22:28:05 +0800

    fix: rename the folder name, same files

---
+++ controllers/employee.controller.js
@@ -0,0 +1,86 @@
+const pool = require('../database/index')
+
+const employeeController = {
+  getEmployee: async (req, res) => {
+    try {
+      const [rows, fields] = await pool.query("SELECT * FROM restaurant_menu.employee")
+      res.json({
+        data: rows
+      })
+    } catch (error) {
+      console.log(error)
+      res.json({
+        state: "error"
+      })
+    }
+  },
+  getEmployeeByID: async (req, res) => {
+    try {
+      const { employee_id } = req.params
+      const sql = "SELECT * FROM restaurant_menu.employee WHERE employee_id = ?"
+      const [rows, fields] = await pool.query(sql, [employee_id])
+      res.json({
+        data: rows
+      })
+    } catch (error) {
+      console.log(error)
+      res.json({
+        state: "error"
+      })
+    }
+  },
+  addEmployee: async (req, res) => {
+    try {
+      const { fullname, position, age, number, email, branchid, years } = req.body
+      const sql = `INSERT INTO restaurant_menu.employee (fullname, position, age, number, email, branch_id, years_of_service, date_started) VALUES (?, ?, ?, ?, ?, ?, ?, NOW())`
+      const [rows, fields] = await pool.query(sql, [fullname, position, age, number, email, branchid, years])
+      res.json({ 
+        data: rows
+      })
+    } catch (error) {
+      console.log(error)
+      res.json({
+        state: "error"
+      })
+    }
+  },
+  updateEmployee: async (req, res) => {
+    try {
+      const { employee_id } = req.params
+      const { fullname, position, age, number, email, branchid, years } = req.body
+      const sql = `UPDATE restaurant_menu.employee SET fullname = ?, position = ?, age = ?, number = ?, email = ?, branch_id = ?, years_of_service = ?, date_updated = NOW() WHERE employee_id = ?`
+      const [rows, fields] = await pool.query(sql, [fullname, position, age, number, email, branchid, years, employee_id])
+      res.json({
+        data: rows
+      })
+    } catch (error) {
+      console.log(error)
+      res.json({
+        state: "error"
+      })
+    }
+  },
+  deleteEmployee: async (req, res) => {
+    try {
+      const { employee_id } = req.params
+      const sql = `DELETE FROM restaurant_menu.employee WHERE employee_id = ?`
+      const [rows, fields] = await pool.query(sql, [employee_id])
+      res.json({
+        data: rows
+      })
+    } catch (error) {
+      console.log(error)
+      res.json({
+        state: "error"
+      })
+    }
+  },
+  // getAllManager: async (req, res) => {
+
+  // },
+  // getAllDecadeOfService: async (req, res) => {
+
+  // }
+}
+
+module.exports = employeeController
---
+++ controllers/user.controller.js
@@ -0,0 +1,47 @@
+const {
+  createUser,
+  getAllUsers,
+  getUserByID,
+  getUserByEmail,
+  updateUser,
+  deleteUser
+} = require("../models/user.service")
+
+const { hashSync, genSaltSync, compareSync } = require("bcrypt");
+const { sign } = require("jsonwebtoken");
+
+module.exports = {
+  createUser: (req, res) => {
+    const body = req.body
+    const salt = genSaltSync(10)
+    body.password = hashSync(body.password, salt)
+    createUser(body, (err, results) => {
+      if(err){
+        console.log(err)
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error"
+        });
+      }
+      return res.json({
+        success: 1,
+        data: results
+      })
+    })
+  },
+  getAllUsers: (req, res) => {
+    getAllUsers((err, results) => {
+      if(err){
+        console.log(err)
+        return res.json({
+          success: 0
+        })
+      }
+      console.log(results)
+      return res.json({
+        success: 1,
+        data: results
+      })
+    })
+  }
+}
