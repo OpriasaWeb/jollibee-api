df790b8eab27e300268cd26ad7a78b0949fc5dbf
Jeremy Opriasa <opriasajbusiness@gmail.com>
2024-01-06 22:51:27 +0800

    fix: modify some codes for debugging

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require("express")
 const app = express()
 
-require("dotenv").config()
+require('dotenv').config()
 
 const userRouter = require("./routes/user.routes")
 const menuRouter = require("./routes/menu.routes")
 const employeeRouter = require("./routes/employee.routes")
 
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended:false }))
 
 app.use("/restapi/restaurant/user", userRouter)
 app.use("/restapi/restaurant/menu", menuRouter)
 app.use("/restapi/restaurant/employee", employeeRouter)
+
+app.use(express.json())
 
 const PORT = process.env.PORT || 3000
 
