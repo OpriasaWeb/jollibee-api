d5bc2cb7cb5b69497836db615ba2c2343abd71e9
Jeremy Opriasa <opriasajbusiness@gmail.com>
2024-01-15 22:33:20 +0800

    fix: add the position and comment out the user for now

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const app = express()
 
 require('dotenv').config()
 
-const userRouter = require("./routes/user.routes")
+// const userRouter = require("./routes/user.routes")
 const menuRouter = require("./routes/menu.routes")
 const employeeRouter = require("./routes/employee.routes")
 const branchRouter = require("./routes/branches.routes")
+const refPositionRouter = require('./routes/ref_positions.routes')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended:false }))
 
-app.use("/restapi/jollibee/user", userRouter)
+// app.use("/restapi/jollibee/user", userRouter)
 app.use("/restapi/jollibee/menu", menuRouter)
 app.use("/restapi/jollibee/employee", employeeRouter)
 app.use("/restapi/jollibee/branch", branchRouter)
+app.use("/restapi/jollibee/position", refPositionRouter)
 
 
 
