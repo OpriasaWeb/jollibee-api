d41e4b6f16382649965e85914c70a852791fefe5
Jeremy Opriasa <opriasajbusiness@gmail.com>
2024-01-16 17:30:47 +0800

    feat: breakfast meals reference has been added to this project

---
+++ controllers/ref_breakfast_sandwiches.controller.js
@@ -0,0 +1,21 @@
+const pool = require('../database/index')
+
+const refBreakfastSandwiches = {
+  addBreakfastSandwiches: async (req, res) => {
+    try {
+      const { breakfast_sandwiches_name } = req.body
+      const sql = `INSERT INTO restaurant_menu.ref_breakfast_sandwiches (breakfast_sandwiches_name) VALUES (?) `
+      const [rows, fields] = await pool.query(sql, [breakfast_sandwiches_name])
+      res.json({
+        data: rows
+      })
+    } catch (error) {
+      console.log(error)
+      res.json({
+        state: "error"
+      })
+    }
+  }
+}
+
+module.exports = refBreakfastSandwiches
