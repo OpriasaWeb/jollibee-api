927413c09a88ffe5256745b7001b5e5e6c68fb89
Jeremy Opriasa <opriasajbusiness@gmail.com>
2024-01-16 16:41:20 +0800

    fix: add the edit and delete functionalities for branch feature of this very project

--- controllers/branches.controller.js
+++ controllers/branches.controller.js
@@ -43,6 +43,38 @@ const branchesController = {
         state: "error"
       })
     }
+  },
+  editBranch: async (req, res) => {
+    try { 
+      const { branch_name, branch_address, branch_telephone, branch_number, branch_employees } = req.body
+      const { branch_id } = req.params
+      const sql = `UPDATE restaurant_menu.jollibee_branch SET branch_name = ?, branch_address = ?, branch_telephone = ?, branch_number = ?, branch_employees = ? WHERE branch_id = ? `
+      const [rows, fields] = await pool.query(sql, [branch_name, branch_address, branch_telephone, branch_number, branch_employees, branch_id])
+      res.json({
+        data: rows
+      })
+      // console.log(rows['affectedRows'])
+    } catch (error) {
+      console.log(error)
+      res.json({
+        state: "error"
+      })
+    }
+  },
+  deleteBranch: async (req, res) => {
+    try {
+      const { branch_id } = req.params
+      const sql = `DELETE FROM restaurant_menu.jollibee_branch WHERE branch_id = ? `
+      const [rows, fields] = await pool.query(sql, [branch_id])
+      res.json({
+        data: rows
+      })
+    } catch (error) {
+      console.log(error)
+      res.json({
+        state: "error"
+      })
+    }
   }
 }
 
--- routes/branches.routes.js
+++ routes/branches.routes.js
@@ -6,6 +6,8 @@ const branchesController = require("../controllers/branches.controller")
 router.get("/", branchesController.getBranches)
 router.get("/:branch_id", branchesController.getBranchByID)
 router.post("/", branchesController.addBranch)
+router.put("/:branch_id", branchesController.editBranch)
+router.delete("/:branch_id", branchesController.deleteBranch)
 
 
 module.exports = router
