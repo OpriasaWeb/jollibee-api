e6e29be4a6ed33f2c13ea29b9c38b908630e37e2
Jeremy Opriasa <opriasajbusiness@gmail.com>
2024-02-06 20:40:59 +0800

    fix: finish the order section

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -28,9 +28,34 @@ const orderController = {
   },
   getOrder: async (req, res) => {
     try {
-      const { employee_id } = req.params
-      const sql = "SELECT fullname, rp.position_name, age, number, email, branch_id, years_of_service FROM employee e LEFT JOIN ref_position rp ON e.position_id = rp.position_id WHERE employee_id = ?"
-      const [rows, fields] = await pool.query(sql, [employee_id])
+      const { queue_id } = req.params
+      const sql = `SELECT o.queue_id, 
+      o.beverage_id, (o.beverage_count * rb.price) AS beverage_total, 
+      o.breakfast_meal_id, (o.breakfast_meal_count * rbm.price) AS breakfast_meal_total, 
+      o.breakfast_sandwich_id, (o.breakfast_sandwich_count * rbs.price) AS breakfast_sandwich_total, 
+      o.burger_id, (o.burger_count * rbrgr.price) AS burger_total, 
+      o.burger_steak_id, (o.burger_steak_count * rbstk.price) AS burger_steak_total, 
+      o.chickenjoy_id, (o.chickenjoy_count * rc.price) AS chickenjoy_total, 
+      o.dessert_id, (o.dessert_count * rd.price) AS dessert_total, 
+      o.noodles_pasta_id, (o.noodles_pasta_count * rnp.price) AS noodles_total, 
+      o.rice_meal_id, (o.rice_meal_count * rrm.price) AS rice_meal_total, 
+      o.side_item_id, (o.side_item_count * rsitms.price) AS side_item_total, 
+      o.special_id, (o.special_count * rsi.price) AS special_total, 
+      o.order_count 
+      FROM orders o 
+      LEFT JOIN ref_beverages rb ON o.beverage_id = rb.beverages_id
+      LEFT JOIN ref_breakfast_meals rbm ON o.breakfast_meal_id = rbm.breakfast_meals_id 
+      LEFT JOIN ref_breakfast_sandwiches rbs ON o.breakfast_sandwich_id = rbs.breakfast_sandwiches_id 
+      LEFT JOIN ref_burger rbrgr ON o.burger_id = rbrgr.burger_id
+      LEFT JOIN ref_burger_steak rbstk ON o.burger_steak_id = rbstk.burger_steak_id
+      LEFT JOIN ref_chickenjoy rc ON o.chickenjoy_id = rc.chickenjoy_id
+      LEFT JOIN ref_desserts rd ON o.dessert_id = rd.desserts_id
+      LEFT JOIN ref_noodles_and_pasta rnp ON o.noodles_pasta_id = rnp.noodles_pasta_id
+      LEFT JOIN ref_rice_meals rrm ON o.rice_meal_id = rrm.rice_meals_id
+      LEFT JOIN ref_side_items rsitms ON o.side_item_id = rsitms.side_items_id
+      LEFT JOIN ref_special_item rsi ON o.special_id = rsi.special_item_id
+      WHERE o.queue_id = ? `
+      const [rows, fields] = await pool.query(sql, [queue_id])
       res.json({
         data: rows
       })
@@ -41,58 +66,73 @@ const orderController = {
       })
     }
   },
-  // getAllOrders: async (req, res) => {
-  //   try {
-  //     const [rows, fields] = await pool.query("SELECT fullname, rp.position_name, age, number, email, branch_id, years_of_service FROM employee e LEFT JOIN ref_position rp ON e.position_id = rp.position_id")
-  //     res.json({
-  //       data: rows
-  //     })
-  //   } catch (error) {
-  //     console.log(error)
-  //     res.json({
-  //       state: "error"
-  //     })
-  //   }
-  // },
-
-  
-  // updateOrder: async (req, res) => {
-  //   try {
-  //     const { employee_id } = req.params
-  //     const { fullname, position, age, number, email, branchid, years } = req.body
-  //     const sql = `UPDATE restaurant_menu.employee SET fullname = ?, position = ?, age = ?, number = ?, email = ?, branch_id = ?, years_of_service = ?, date_updated = NOW() WHERE employee_id = ?`
-  //     const [rows, fields] = await pool.query(sql, [fullname, position, age, number, email, branchid, years, employee_id])
-  //     res.json({
-  //       data: rows
-  //     })
-  //   } catch (error) {
-  //     console.log(error)
-  //     res.json({
-  //       state: "error"
-  //     })
-  //   }
-  // },
-  // voidOrder: async (req, res) => {
-  //   try {
-  //     const { employee_id } = req.params
-  //     const sql = `DELETE FROM restaurant_menu.employee WHERE employee_id = ?`
-  //     const [rows, fields] = await pool.query(sql, [employee_id])
-  //     res.json({
-  //       data: rows
-  //     })
-  //   } catch (error) {
-  //     console.log(error)
-  //     res.json({
-  //       state: "error"
-  //     })
-  //   }
-  // },
-  // getAllManager: async (req, res) => {
-
-  // },
-  // getAllDecadeOfService: async (req, res) => {
-
-  // }
+  voidOrder: async (req, res) => {
+    try {
+      const { order_id, queue_id } = req.params
+      const sql = `UPDATE restaurant_menu.orders SET status = 3, is_void = 1 WHERE order_id = ? AND queue_id = ?`
+      const [rows, fields] = await pool.query(sql, [order_id, queue_id])
+      res.json({
+        data: rows
+      })
+    } catch (error) {
+      console.log(error)
+      res.json({
+        state: "error"
+      })
+    }
+  },
+  updateOrder: async (req, res) => {
+    try {
+      const { order_id, queue_id } = req.params
+      const { beverage_id, beverage_count,
+              breakfast_meal_id, breakfast_meal_count,
+              breakfast_sandwich_id, breakfast_sandwich_count,
+              burger_id, burger_count,
+              burger_steak_id, burger_steak_count,
+              chickenjoy_id, chickenjoy_count,
+              dessert_id, dessert_count,
+              noodles_pasta_id, noodles_pasta_count,
+              rice_meal_id, rice_meal_count,
+              side_item_id, side_item_count,
+              special_id, special_count
+            } = req.body
+      const sql = `UPDATE restaurant_menu.orders 
+      SET beverage_id = ?, beverage_count = ?, 
+      breakfast_meal_id = ?, 	breakfast_meal_count = ?, 
+      breakfast_sandwich_id = ?, breakfast_sandwich_count = ?,
+      burger_id = ?, burger_count = ?,
+      burger_steak_id = ?, 	burger_steak_count = ?,
+      chickenjoy_id = ?, chickenjoy_count = ?,
+      dessert_id = ?, dessert_count = ?,
+      noodles_pasta_id = ?, noodles_pasta_count = ?,
+      rice_meal_id = ?, rice_meal_count = ?,
+      side_item_id = ?, 	side_item_count = ?,
+      special_id = ?, special_count = ?
+      date_ordered = NOW() 
+      WHERE order_id = AND queue_id = ?`
+      const [rows, fields] = await pool.query(sql, [
+        beverage_id, beverage_count,
+        breakfast_meal_id, breakfast_meal_count,
+        breakfast_sandwich_id, breakfast_sandwich_count,
+        burger_id, burger_count,
+        burger_steak_id, burger_steak_count,
+        chickenjoy_id, chickenjoy_count,
+        dessert_id, dessert_count,
+        noodles_pasta_id, noodles_pasta_count,
+        rice_meal_id, rice_meal_count,
+        side_item_id, side_item_count,
+        special_id, special_count,
+        order_id, queue_id])
+      res.json({
+        data: rows
+      })
+    } catch (error) {
+      console.log(error)
+      res.json({
+        state: "error"
+      })
+    }
+  },
 }
 
 module.exports = orderController
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const orderController = require("../controllers/order.controller")
 
 router.post("/", orderController.orderMenu)
+router.get("/:queue_id", orderController.getOrder)
+router.put("/:order_id/:queue_id", orderController.voidOrder)
 // router.get("/", employeeController.getEmployee)
 // router.get("/:employee_id", employeeController.getEmployeeByID)
-// router.put("/:employee_id", employeeController.updateEmployee)
+
 // router.delete("/:employee_id", employeeController.deleteEmployee)
 
 module.exports = router
